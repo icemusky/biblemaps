6618026bfe0673af780a9ebbd0bdf9d7799fd209
Eric <icemusky@126.com>
2017-08-05 00:32:34 +0800

    笔记本提交更新

--- app/src/main/java/eric/cn/com/biblemaps/MainActivity.java
+++ app/src/main/java/eric/cn/com/biblemaps/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         initView();
